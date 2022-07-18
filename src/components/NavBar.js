f880ea61e6cdd6495e2fea33a4b17be2763ddb6d
JuanLeonelC <juanleonelcd@gmail.com>
2022-07-18 18:56:27 -0300

    Primer desafio

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return(
+        // Fragment
+        <div>
+            <ul>
+                <li><a href="#">Qwerty's</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Sobre Nosotros</a></li>
+                <li><a href="#">Contactanos</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar;
